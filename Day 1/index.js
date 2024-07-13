309352cff793ca5c5d37cb6c68495b554af131a9
codax69 <pritbusiness03@gmail.com>
2024-07-13 23:18:54 +0530

    Day 1 challenge Completed

---
+++ Day 1/index.js
@@ -0,0 +1,44 @@
+// Activity 1
+// Task:1
+var num = 16
+console.log(num)
+
+// Task:2
+let str = "Hello I am Pritesh Patel"
+console.log(str)
+
+// Activity 2
+// Task:3
+const NotaBoolean = true
+console.log(NotaBoolean)
+
+// Activity 4
+// Task 4
+let newNum = 2005
+let newStr = "Hello Challenge accepted"
+let notaType = false
+let arr = [1,2,3,4,5,6]
+let obj = {
+    name:"Pritesh",
+    age:19
+}
+console.log(typeof newNum)
+console.log(typeof newStr)
+console.log(typeof notaType)
+console.log(typeof arr)
+console.log(typeof obj)
+
+// Task 5
+let initialValue = "Hello"
+console.log(initialValue)
+initialValue = "Hey"
+
+// Activity 5:
+const value = 40
+console.log(value)
+
+try {
+    value=60
+} catch (error) {
+    console.log(("error:",error))
+}
