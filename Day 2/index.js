233e32773bf56cb29388b68da40d38b39eb15864
codax69 <pritbusiness03@gmail.com>
2024-07-14 13:09:23 +0530

    Day 2 Challenge Completed

---
+++ Day 2/index.js
@@ -0,0 +1,74 @@
+// Activity 1 : Arithmetic Operations
+// Task 1
+const sum = (a, b) => {
+  console.log(a + b);
+};
+sum(16 + 10);
+
+//Task 2
+const subtract = (x, y) => {
+  console.log(x - y);
+};
+subtract(12, 4);
+
+//Task 3
+const multiply = (c, d) => {
+  console.log(c * d);
+};
+multiply(456 * 56);
+
+//Task 4
+const divide = (p, q) => {
+  console.log(p, q);
+};
+divide(8546, 9);
+
+//Activity 2: Assignment Operators
+//Task 6
+let num = 15;
+console.log("num:", num);
+num += 5;
+console.log("NewNum:", num);
+
+// Task 6
+let subtractNum = 45;
+console.log("sunNum:", subtractNum);
+
+subtractNum -= 8;
+console.log("newSunNum:", subtractNum);
+
+//Activity 3: Comparison Operators
+
+//Task 8
+// Task 9
+// Task 10
+const compar = (a, b) => {
+  console.log(a < b);
+  console.log(a <= b);
+  console.log(a == b);
+  console.log(a === b);
+};
+compar(10, 14);
+console.log("//");
+compar(10, "10");
+
+// Activity 4: Logical Operators
+//Task 11
+//Task 12
+console.log("")
+const logicalOperation = (x, y) => {
+  console.log(x && y);
+  console.log(x || y);
+};
+logicalOperation(true, false);
+logicalOperation(true, true);
+
+//Task 13
+const fail = !true
+console.log("Fail:",fail)
+
+//Activity 5: Ternary Operator
+//Task 14
+
+console.log(fail ? fail:true)
+
